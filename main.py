05d50680191ed21913565f3f3a4920840a79e829
Ian Maddox <github@ianmaddox.com>
2023-12-27 02:10:06 -0800

    incremental

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     apply_studio_style()
     st.title("Moving Along SMB Website Generator Demo")
     st.markdown("Prototype demonstration of some of the amazing technologies on offer by AI21, created for Moving Along LLC")
-    st.markdown("Demo by Ian Maddox, Solutions Architect, AI21 Labs")
+    st.markdown("Demo by Ian Maddox, AI21 Solutions Architect")
     st.markdown("December 27, 2023, ian@ai21.com")
